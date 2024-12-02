940b5cdbf8282844b5ab16dbfb322ace5f43c224
TGVM <tgvm96@gmail.com>
2024-12-02 15:16:23 -0300

    Feat Mouse Movement

--- DOTS RTS/Assets/Scripts/Systems/UnitMoverSystem.cs
+++ DOTS RTS/Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -2,6 +2,7 @@ using Unity.Burst;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Unity.Physics;
 
 partial struct UnitMoverSystem : ISystem
 {
@@ -10,14 +11,35 @@ partial struct UnitMoverSystem : ISystem
     public void OnUpdate(ref SystemState state)
     {
         foreach((RefRW<LocalTransform> localTransform,
-            RefRO<MoveSpeed> moveSpeed) 
+            RefRO<MoveSpeed> moveSpeed,
+            RefRW<PhysicsVelocity> physicsVelocity) 
             in SystemAPI.Query<
                 RefRW<LocalTransform>, 
-                RefRO<MoveSpeed>>()){ 
-            localTransform.ValueRW.Position = 
-                localTransform.ValueRO.Position +
-                new float3(moveSpeed.ValueRO.value,0,0) * 
-                SystemAPI.Time.DeltaTime;
+                RefRO<MoveSpeed>,
+                RefRW<PhysicsVelocity>>()){
+
+            float3 targetPosition = MouseWorldPosition.Instance.GetPosition();
+
+            float3 moveDirection = targetPosition - 
+                localTransform.ValueRO.Position;
+            moveDirection = math.normalize(moveDirection);
+
+            float rotationSpeed = 10f;
+            localTransform.ValueRW.Rotation =
+                math.slerp(localTransform.ValueRO.Rotation,
+                quaternion.LookRotation(moveDirection, math.up()),
+                SystemAPI.Time.DeltaTime * rotationSpeed);
+
+            physicsVelocity.ValueRW.Linear = 
+                moveDirection *
+                moveSpeed.ValueRO.value;
+
+            physicsVelocity.ValueRW.Angular = float3.zero;
+
+            //localTransform.ValueRW.Position +=
+            //    moveDirection *
+            //    moveSpeed.ValueRO.value *
+            //    SystemAPI.Time.DeltaTime;
         }
     }
 
