35f38096458338da544c5d4a83bf1c779bb3708a
TGVM <tgvm96@gmail.com>
2025-02-06 14:49:11 -0300

    Feat: Unit Selector

--- DOTS RTS/Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
+++ DOTS RTS/Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
@@ -1,11 +1,76 @@
 using Unity.Collections;
+using System;
 using Unity.Entities;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Unity.Transforms;
 
-public class UnitSelectionManager : MonoBehaviour
-{
+public class UnitSelectionManager : MonoBehaviour {
+    public static UnitSelectionManager Instance { get; private set; }
+
+    public event EventHandler OnSelectionAreaStart;
+    public event EventHandler OnSelectionAreaEnd;
+
+    private Vector2 selectionStartMousePosition;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            selectionStartMousePosition = Input.mousePosition;
+
+
+            OnSelectionAreaStart?.Invoke(this, EventArgs.Empty);
+        }
+        if (Input.GetMouseButtonUp(0)) { 
+            Vector2 selectionEndMousePosition = Input.mousePosition;
+
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp)
+                .WithAll<Selected>()
+                .Build(entityManager);
+
+            NativeArray<Entity> entityArray =
+                entityQuery.ToEntityArray(Allocator.Temp);
+
+            for(int i = 0; i < entityArray.Length; i++)
+            {
+                entityManager.SetComponentEnabled<Selected>(entityArray[i], false);
+            }
+
+            entityQuery = new EntityQueryBuilder(Allocator.Temp)
+                .WithAll<LocalTransform, Unit>()
+                .WithPresent<Selected>()
+                .Build(entityManager);
+
+
+            entityArray =
+                entityQuery.ToEntityArray(Allocator.Temp);
+
+            NativeArray<LocalTransform> LocalTransformArray =
+                entityQuery.ToComponentDataArray<LocalTransform>(Allocator.Temp);
+
+            Rect selectionAreaRect = GetSelectionAreaRect();
+            for (int i = 0; i < LocalTransformArray.Length; i++)
+            {
+                LocalTransform unitLocalTransform = LocalTransformArray[i];
+                Vector2 unitScreenPosition = Camera.main.WorldToScreenPoint(unitLocalTransform.Position);
+                if(selectionAreaRect.Contains(unitScreenPosition))
+                {
+                    //Unit is inside the area
+                    entityManager.SetComponentEnabled<Selected>(entityArray[i], true);
+                }
+            }
+
+
+            OnSelectionAreaEnd?.Invoke(this, EventArgs.Empty);
+        }
+
         if (Input.GetMouseButtonDown(1))
         {
             Vector3 mouseWorldPosition = MouseWorldPosition.Instance.GetPosition();
@@ -29,6 +94,28 @@ public class UnitSelectionManager : MonoBehaviour
             }
             entityQuery.CopyFromComponentDataArray(unitMoverArray);
         }
+    }
+
+    public Rect GetSelectionAreaRect()
+    {
+        Vector2 selectionEndMousePosition = Input.mousePosition;
+
+        Vector2 lowerLeftCorner = new Vector2(
+            Mathf.Min(selectionStartMousePosition.x, selectionEndMousePosition.x),
+            Mathf.Min(selectionStartMousePosition.y, selectionEndMousePosition.y)
+            );
+
+        Vector2 upperRightCorner = new Vector2(
+            Mathf.Max(selectionStartMousePosition.x, selectionEndMousePosition.x),
+            Mathf.Max(selectionStartMousePosition.y, selectionEndMousePosition.y)
+            );
+
+        return new Rect(
+            lowerLeftCorner.x,
+            lowerLeftCorner.y,
+            upperRightCorner.x - lowerLeftCorner.x,
+            upperRightCorner.y - lowerLeftCorner.y
+            );
     }
 
 }
