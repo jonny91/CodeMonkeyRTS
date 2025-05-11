5d0cc75e7271b0d7621540d0e589b9c5d72f5955
洪金敏 <jonny.hong91@gmail.com>
2025-05-11 23:54:05 +0800

    角色增加朝向鼠标位置移动 (存在burst错误)

---
+++ MonkeyRTS/Assets/Scripts/MonoBehaviours/MouseWorldPosition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MouseWorldPosition : MonoBehaviour
+{
+    public static MouseWorldPosition Instance { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public Vector3 GetPosition()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        var plane = new Plane(Vector3.up, Vector3.zero);
+        if (plane.Raycast(ray, out var distance))
+        {
+            return ray.GetPoint(distance);
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+}
---
+++ MonkeyRTS/Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Transforms;
+
+public partial struct UnitMoverSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach (var (localTransform, moveSpeed, physicsVelocity) in
+                 SystemAPI.Query<RefRW<LocalTransform>, RefRO<MoveSpeed>, RefRW<PhysicsVelocity>>())
+        {
+            var targetPosition = (float3)MouseWorldPosition.Instance.GetPosition();
+            var moveDirection = math.normalize(targetPosition - localTransform.ValueRO.Position);
+            // localTransform.ValueRW.Rotation = quaternion.LookRotationSafe(moveDirection, math.up());
+            var rotationSpeed = 10f;
+            localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRW.Rotation,
+                quaternion.LookRotationSafe(moveDirection, math.up()), rotationSpeed * SystemAPI.Time.DeltaTime);
+            physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.Value;
+            physicsVelocity.ValueRW.Angular = 0;
+            // localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.Value * SystemAPI.Time.DeltaTime;
+        }
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+    }
+}
